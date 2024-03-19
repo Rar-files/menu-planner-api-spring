167fc8b3a294f1bea301212074f8650e2daf7472
rar-files <dwidlak@outlook.com>
2024-03-19 05:30:37 +0100

    Created: IngredientController

--- src/main/java/menuplanner/rary/re/apispring/repository/meal/IngredientRepository.java
+++ src/main/java/menuplanner/rary/re/apispring/repository/meal/IngredientRepository.java
@@ -2,7 +2,13 @@ package menuplanner.rary.re.apispring.repository.meal;
 
 import menuplanner.rary.re.apispring.model.meal.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface IngredientRepository extends JpaRepository<Ingredient, Integer> { }
+public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
+    @Query("select i from Ingredient i where i.meal.id = :id")
+    List<Ingredient> getIngredientsByMealId(int id);
+}
